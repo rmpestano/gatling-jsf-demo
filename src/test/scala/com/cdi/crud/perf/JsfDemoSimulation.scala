4b526bae2d13b4fe3d28aeae0243f76507926e76
rmpestano <bigboss666>
2015-05-15 00:15:28 -0300

    first commit

---
+++ src/test/scala/com/cdi/crud/perf/JsfDemoSimulation.scala
@@ -0,0 +1,91 @@
+package com.cdi.crud.perf
+
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import io.gatling.core.session.Expression
+
+class CdiCrudSimulation extends Simulation {
+
+
+  val httpProtocol = http
+    .baseURL("http://localhost:8080/cdi-crud")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8,application/json;charset=utf-8")
+    .acceptEncodingHeader("gzip, deflate")
+    .inferHtmlResources()
+    .connection( """keep-alive""")
+    /*.contentTypeHeader("application/json; charset=UTF-8")*/
+    .contentTypeHeader("*/*")
+    .acceptLanguageHeader("pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 6.3; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0")
+
+
+
+  //JSF requests and scenarios
+
+  val jsf_headers = Map(
+    "Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Pragma" -> "no-cache",
+    "X-Requested-With" -> "XMLHttpRequest")
+
+  val jsfViewStateCheck = css("input[name='javax.faces.ViewState']")
+    .saveAs("viewState")
+
+  val jsfPartialViewStateCheck = xpath("//update[contains(@id,'javax.faces.ViewState')]")
+    .saveAs("viewState")
+
+  def jsfGet(name: String, url: Expression[String]) =
+    http(name)
+      .get(url)
+      .check(jsfViewStateCheck)
+
+  def jsfPost(name: String, url: Expression[String]) = http(name)
+    .post(url)
+    .formParam("javax.faces.ViewState", "${viewState}")
+    .check(jsfPartialViewStateCheck)
+
+  def openDialogRequest = jsfPost("request_open_dialog","/index.faces?dswid=4728")
+    .header("Faces-Request", "partial/ajax")
+    .formParam("javax.faces.partial.ajax", "true")
+    .formParam("javax.faces.source", "openLogin")
+    .formParam("javax.faces.partial.execute", "openLogin")
+    .formParam("javax.faces.partial.render", "logonForm:logonPanel")
+    .formParam("openLogin", "openLogin")
+    .formParam("javax.faces.ClientWindow", "4728")
+    .check(status.is(200))
+
+  def doLogonRequest =
+    jsfPost("request_logon","/index.faces?dswid=4728")
+      .header("Faces-Request", "partial/ajax")
+      .formParam("javax.faces.partial.ajax", "true")
+      .formParam("javax.faces.source", "logonForm:btLogin")
+      .formParam("javax.faces.partial.execute", "@all")
+      .formParam("logonForm:btLogin", "logonForm:btLogin")
+      .formParam("logonForm", "logonForm")
+      .formParam("logonForm:user", "admin")//input text
+      .formParam("javax.faces.ClientWindow", "4728")
+      .check(status.is(200))
+
+
+  val loginScenario = scenario("login")
+    .exec(
+      jsfGet("saveState","/index.faces")
+      .resources(http("request_resources").get( "/"))
+     .check(status.is(200))
+    )
+    .exec(openDialogRequest)
+    .pause(2)
+    .exec(doLogonRequest)
+    .pause(1)
+
+  setUp(
+    loginScenario.inject( atOnceUsers(1) )
+  )
+  .protocols(httpProtocol)
+    .assertions(
+      global.successfulRequests.percent.greaterThan(95)
+    )
+
+
+}
---
+++ src/test/scala/com/rmpestano/gatling/perf/JsfDemoSimulation.scala
@@ -0,0 +1,107 @@
+package com.rmpestano.gatling.perf
+
+
+import _root_.io.gatling.http.Predef._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import io.gatling.core.session.Expression
+
+class JsfDemoSimulation extends Simulation {
+
+
+  val httpProtocol = http
+    .baseURL("http://www.primefaces.org/showcase")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate")
+    .inferHtmlResources()
+    .connection( """keep-alive""")
+    .contentTypeHeader("*/*")
+    .acceptLanguageHeader("pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 6.3; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0")
+
+
+  //JSF requests and scenarios
+
+  val jsf_headers = Map(
+    "Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Pragma" -> "no-cache",
+    "X-Requested-With" -> "XMLHttpRequest")
+
+  val jsfViewStateCheck = css("input[name='javax.faces.ViewState']", "value")
+    .saveAs("viewState")
+
+  val jsfPartialViewStateCheck = xpath("//update[contains(@id,'ViewState')]")
+    .saveAs("viewState")
+
+  val ajaxButtonIdCheck = css("button[id$='ajax']", "id")
+    .saveAs("btAjax")
+
+  val nonAjaxButtonIdCheck = css("button[id$='nonAjax']", "id")
+    .saveAs("btNonAjax")
+
+  val formIdCheck = css("form", "id")
+    .saveAs("form")
+
+  def jsfGet(name: String, url: Expression[String]) =
+    http(name)
+      .get(url)
+      .check(jsfViewStateCheck)
+
+  def jsfPost(name: String, url: Expression[String]) = http(name)
+    .post(url)
+    .formParam("javax.faces.ViewState", "${viewState}")
+    .check(jsfViewStateCheck)
+
+  def jsfPartialPost(name: String, url: Expression[String]) = http(name)
+    .post(url)
+    .header("Faces-Request", "partial/ajax")
+    .formParam("javax.faces.partial.ajax", "true")
+    .formParam("javax.faces.ViewState", "${viewState}")
+    .check(jsfPartialViewStateCheck)
+
+  def ajaxButtonCall = jsfPartialPost("request_ajax_button", "/ui/button/commandButton.xhtml")
+    .formParam("javax.faces.source", "${btAjax}")
+    .formParam("javax.faces.partial.execute", "@all")
+    .formParam("${btAjax}", "${btAjax}")
+    .formParam("${form}", "${form}")
+    .check(status.is(200))
+
+  def normalButtonRequest =
+    jsfPost("normal_request", "/ui/button/commandButton.xhtml")
+      .formParam("${btNonAjax}", "${btNonAjax}")
+      .formParam("${form}", "${form}")
+      .check(status.is(200))
+
+
+  val jsfScenario = scenario("jsf")
+    .exec(
+      jsfGet("saveState", "/ui/button/commandButton.xhtml")
+        .check(status.is(200), ajaxButtonIdCheck, formIdCheck, nonAjaxButtonIdCheck)
+    )
+    .pause(2)
+
+    //just prints the session
+    .exec(session => {
+    println(session)
+    session
+  })
+    .pause(100 milliseconds)
+    .exec(normalButtonRequest)
+    .pause(1)
+
+    .exec(session => {
+    println(session)
+    session
+  })
+
+  setUp(
+    jsfScenario.inject(atOnceUsers(5))
+  )
+    .protocols(httpProtocol)
+    .assertions(
+      global.successfulRequests.percent.greaterThan(95)
+    )
+
+
+}
